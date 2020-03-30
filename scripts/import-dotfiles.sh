da5cbcae1692eab5fc75799cf6f11608c8f628a1
Timofeev Mikhail <desentcare45@gmail.com>
2020-03-31 00:44:06 +0500

    Add working script for importing dotfiles

--- scripts/import-dotfiles.sh
+++ scripts/import-dotfiles.sh
@@ -1,3 +1,15 @@
-cd
-pwd
-mkdir TESTDIR
+type git > /dev/null || exec echo "missing dependences: git not found"
+
+[ -f $HOME/.gitignore ] || echo ".dotfiles.git" >> \
+	$HOME/.gitignore && echo "gitignore created" 
+
+[ -d $HOME/.dotfiles.git ] || \
+git clone --bare "https://github.com/des45/dotfiles.git" $HOME/.dotfiles.git
+
+alias dotfiles="git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME"
+
+dotfiles checkout > /dev/null || exec echo "checkout failed"
+
+dotfiles config --local status.showUntrackedFiles no
+
+echo "dotfiles was successfully imported"
