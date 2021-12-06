1053c19091fa2c3f88e9b05c223e16917468ff5e
ashuya <despersonal45@gmail.com>
2021-12-06 19:53:09 +0500

    updated dotfiles setup

---
+++ tools/setup-dotfiles.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/sh
+type apt-get > /dev/null && (
+apt update; sudo apt-get upgrade
+apt install --upgrade neovim vim git fish tmux python3 
+)
+type git > /dev/null || exec echo "missing dependences: git not found"
+
+[ -d $HOME/.cfg ] || \
+git clone --bare "git@github.com:ashuya/dotfiles.git" $HOME/.cfg
+
+config="git --git-dir=$HOME/.cfg --work-tree=$HOME"
+
+$config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} |
+    xargs -I{} rm {} -rf
+$config checkout
+
+$config config --local status.showUntrackedFiles no
+
+echo \
+"everything set up \
+Restart your shell"
